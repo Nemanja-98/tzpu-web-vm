6fcbf8ee48bab7a8a65f7fb7bf556e7461980200
Aleksandar Milosavljevic <milosavljevic@elfak.rs>
2021-02-01 20:44:07 +0100

    Add material ui and virtual machine component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { VirtualMachineComponent } from './components/virtual-machine/virtual-machine.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VirtualMachineComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/virtual-machine/virtual-machine.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-virtual-machine',
+  templateUrl: './virtual-machine.component.html',
+  styleUrls: ['./virtual-machine.component.css']
+})
+export class VirtualMachineComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
