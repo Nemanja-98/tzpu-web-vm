43d135eba0c9d7fd651b96addf78c40707f528a4
Aleksandar Milosavljevic <milosavljevic@elfak.rs>
2021-02-01 22:04:31 +0100

    Add virtual machines with examples

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,21 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { VirtualMachineComponent } from './components/virtual-machine/virtual-machine.component';
 
+import { MatCheckboxModule } from '@angular/material/checkbox'; 
+import { MatCardModule } from '@angular/material/card';
+
+
 @NgModule({
   declarations: [
     AppComponent,
-    VirtualMachineComponent
+    VirtualMachineComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCheckboxModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
